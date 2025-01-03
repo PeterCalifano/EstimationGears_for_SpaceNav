2a52088348a2893bbbd92dea610999258feb212c
PeterC <petercalifano.gs@gmail.com>
2025-01-04 00:15:52 +0100

    Change variables names

--- src/SRIFtemplates/GivensRotSRIF.m
+++ src/SRIFtemplates/GivensRotSRIF.m
@@ -1,19 +1,27 @@
-function [o_dxPost, o_dSRInfoMatPost, o_dInfoVecPost, o_dErrorVec, o_dSqrtPxPost, o_dJcost] = GivensRotSRIF(i_dxPrior, ...
-    i_dSRInfoMatPrior, ...
-    i_dYobs, ...
-    i_dHobsMatrix, ...
-    i_bNO_PRIOR_INFO, ...
-    i_bRUN_WHITENING, ...
-    i_dMeasCovSR) %#codegen
+function [dxPost, dSRInfoMatPost, dInfoVecPost, dErrorVec, dSqrtPxPost, dJcost] = GivensRotSRIF(dxPrior, ...
+                                                                                                dSRInfoMatPrior, ...
+                                                                                                dYobs, ...
+                                                                                                dHobsMatrix, ...
+                                                                                                bNO_PRIOR_INFO, ...
+                                                                                                bRUN_WHITENING, ...
+                                                                                                dMeasCovSR) %#codegen
+arguments
+    dxPrior
+    dSRInfoMatPrior
+    dYobs
+    dHobsMatrix
+    bNO_PRIOR_INFO
+    bRUN_WHITENING
+    dMeasCovSR
+end
 %% PROTOTYPE
-% [o_dxPost, o_dSRInfoMatPost, o_dInfoVecPost, o_dErrorVec, o_dSqrtPxPost, o_dJcost] = 
-%                                      GivensRotSRIF(i_dxPrior, ...
-%                                                    i_dSRInfoMatPrior, ...
-%                                                    i_dYobs, ...
-%                                                    i_dHobsMatrix, ...
-%                                                    i_bNO_PRIOR_INFO,...
-%                                                    i_bRUN_WHITENING,...
-%                                                    i_dMeasCovSR)
+% [dxPost, dSRInfoMatPost, dInfoVecPost, dErrorVec, dSqrtPxPost, dJcost] = GivensRotSRIF(dxPrior, ...
+%                                                                                        dSRInfoMatPrior, ...
+%                                                                                        dYobs, ...
+%                                                                                        dHobsMatrix, ...
+%                                                                                        bNO_PRIOR_INFO, ...
+%                                                                                        bRUN_WHITENING, ...
+%                                                                                        dMeasCovSR) %#codegen
 % -------------------------------------------------------------------------------------------------------------
 %% DESCRIPTION
 % Function providing the Least Squares solution for the Observation update in Square Root Information 
@@ -64,33 +72,31 @@ function [o_dxPost, o_dSRInfoMatPost, o_dInfoVecPost, o_dErrorVec, o_dSqrtPxPost
 % [-]
 % -------------------------------------------------------------------------------------------------------------
 %% Future upgrades
-% 1) Code generation test. Ensure that all the necessary variables for the output are allocated.
-% 2) Add function arguments+
-% 3) Rework for improved memory and computation speed
+% 1) Rework for improved memory and computation speed
 % -------------------------------------------------------------------------------------------------------------
 
 %% Function code
 
 % INPUT INTERFACE
 % Get sizes of arrays and checks
-Nx = size(i_dxPrior, 1);
-Ny = size(i_dYobs, 1);
+Nx = size(dxPrior, 1);
+Ny = size(dYobs, 1);
 
-assert(Nx == size(i_dSRInfoMatPrior, 1) && Nx == size(i_dSRInfoMatPrior, 2), 'Mean estimate and Covariance sizes do not match!');
-assert(Nx == size(i_dHobsMatrix, 2) && Ny == size(i_dHobsMatrix, 1), 'State, residual vectors, Observation matrix sizes are not consistent!')
-assert( size(i_dMeasCovSR,2) == size(i_dYobs, 1) );
+assert(Nx == size(dSRInfoMatPrior, 1) && Nx == size(dSRInfoMatPrior, 2), 'Mean estimate and Covariance sizes do not match!');
+assert(Nx == size(dHobsMatrix, 2) && Ny == size(dHobsMatrix, 1), 'State, residual vectors, Observation matrix sizes are not consistent!')
+assert( size(dMeasCovSR,2) == size(dYobs, 1) );
 
 % Initialize arrays
-o_dJcost = 0.0;
+dJcost = 0.0;
 
-if i_bRUN_WHITENING
+if bRUN_WHITENING
     % Measurements
-    i_dYobs = i_dMeasCovSR\i_dYobs;
-    i_dHobsMatrix = i_dMeasCovSR\i_dHobsMatrix;
+    dYobs = dMeasCovSR\dYobs;
+    dHobsMatrix = dMeasCovSR\dHobsMatrix;
 end
 
 %% INITIALIZATION ROUTINE
-if i_bNO_PRIOR_INFO == true
+if bNO_PRIOR_INFO == true
     % Initialize without prior information
     InfoD = eps * eye(Nx);
     InfoU = eye(Nx);
@@ -100,69 +106,69 @@ else
     InfoD = zeros(Nx);
     invD = zeros(Nx);
     for idn = 1:Nx
-        InfoD(idn, idn) = i_dSRInfoMatPrior(idn, idn) * i_dSRInfoMatPrior(idn, idn); % dii = Rii^2
+        InfoD(idn, idn) = dSRInfoMatPrior(idn, idn) * dSRInfoMatPrior(idn, idn); % dii = Rii^2
         invD(idn, idn) = 1/sqrt(InfoD(idn, idn));
     end
-    InfoU = invD * i_dSRInfoMatPrior; % D^(-1/2)*R;
-    InfoVec = InfoU * i_dxPrior; % U*x
+    InfoU = invD * dSRInfoMatPrior; % D^(-1/2)*R;
+    InfoVec = InfoU * dxPrior; % U*x
 end
 
 % Initialize output
-o_dErrorVec = zeros(Ny, 1);
+dErrorVec = zeros(Ny, 1);
 
 % SQUARE ROOT FREE GIVENS ROTATIONS ALGORITHM
 % Nested loops
 for idk = 1:1:Ny
     deltaTmp = 1.0;
     for idi = 1:1:Nx
-        if i_dHobsMatrix(idk, idi) == 0
+        if dHobsMatrix(idk, idi) == 0
             continue; % No operation required
         end
         % Entry of Dpost diagonal
-        DnewEntry = InfoD(idi, idi) + deltaTmp * i_dHobsMatrix(idk, idi) * i_dHobsMatrix(idk, idi);
+        DnewEntry = InfoD(idi, idi) + deltaTmp * dHobsMatrix(idk, idi) * dHobsMatrix(idk, idi);
         % Compute Sbar (Check if it forms the Square Root Covariance) S(idi, idk)
-        SbarTmp = deltaTmp * i_dHobsMatrix(idk, idi) / DnewEntry;
+        SbarTmp = deltaTmp * dHobsMatrix(idk, idi) / DnewEntry;
         % Compute temporary residual
-        yNewTmp = i_dYobs(idk) - InfoVec(idi) * i_dHobsMatrix(idk, idi);
+        yNewTmp = dYobs(idk) - InfoVec(idi) * dHobsMatrix(idk, idi);
         % Add Information in Information vector
         InfoVec(idi) = InfoVec(idi) + yNewTmp * SbarTmp;
         % Replace entries in i_dYobs, deltaTmp and Dpost
-        i_dYobs(idk) = yNewTmp;
+        dYobs(idk) = yNewTmp;
         deltaTmp = deltaTmp * InfoD(idi, idi)/DnewEntry;
         InfoD(idi, idi) = DnewEntry;
 
         for idj = idi+1:1:Nx
             % Hobsnew may be a scalar instead of matrix. It seems that it
             % only serves as temporary storage
-            HobsNewTmp = i_dHobsMatrix(idk, idj) - InfoU(idi, idj) * i_dHobsMatrix(idk, idi);
+            HobsNewTmp = dHobsMatrix(idk, idj) - InfoU(idi, idj) * dHobsMatrix(idk, idi);
             % Update entry of U matrix
             InfoU(idi, idj) = InfoU(idi, idj) + HobsNewTmp * SbarTmp; % SbarTmp is likely S(idk, idj) TBC
             % Update entry of Observation matrix
-            i_dHobsMatrix(idk, idj) = HobsNewTmp;
+            dHobsMatrix(idk, idj) = HobsNewTmp;
 
         end % info matrix columns j innermost loop
     end % state vector i inner loop
-    o_dErrorVec(idk) = sqrt(deltaTmp) * i_dYobs(idk);
+    dErrorVec(idk) = sqrt(deltaTmp) * dYobs(idk);
 end % residuals k outer loop
 
 % STATE VECTOR BACKSUBSTITUTION
-o_dxPost = InfoU\InfoVec;
+dxPost = InfoU\InfoVec;
 
 % OUTPUT INTERFACE
-o_dSRInfoMatPost = sqrt(InfoD) * InfoU;
-o_dInfoVecPost = o_dSRInfoMatPost * o_dxPost;
+dSRInfoMatPost = sqrt(InfoD) * InfoU;
+dInfoVecPost = dSRInfoMatPost * dxPost;
 
 if nargout > 4
     if nargout > 5
         % Cost function value
         for idk = 1:Ny
-            o_dJcost = o_dJcost + o_dErrorVec(idk) * o_dErrorVec(idk);
+            dJcost = dJcost + dErrorVec(idk) * dErrorVec(idk);
         end
     end
     % Compute Square Root covariance matrix
-    o_dSqrtPxPost = eye(Nx)/(o_dSRInfoMatPost);
+    dSqrtPxPost = eye(Nx)/(dSRInfoMatPost);
 else
-    o_dSqrtPxPost = zeros(Nx);
+    dSqrtPxPost = zeros(Nx);
 end
 
 end
