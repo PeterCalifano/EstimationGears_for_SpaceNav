d8a0f4ab3113f7cd945012991ca41bc39234f28a
PeterC <petercalifano.gs@gmail.com>
2025-02-04 18:16:44 +0100

    Implement function to compute camera poses relative to one anchor pose in the window

---
+++ matlab/sharedFiltersModules/observationModels/ComputeCamRelPoses.m
@@ -0,0 +1,81 @@
+function [dRelPos_CkFromCi_Ci, dDCM_CiFromCk, ...
+    dPositionCk_NavFrame, dDCM_NavFrameFromCk] = ComputeCamRelPoses(dDCM_NavFrameFromC, ...
+                                                                    dPositionCam_NavFrame, ...
+                                                                    ui32EstimationTimeID, ...
+                                                                    ui32NumOfPoses, ...
+                                                                    ui32MaxNumOfPoses)%#codegen
+arguments
+    % DEVNOTE: comment validation args for speed
+    dDCM_NavFrameFromC          (3,3,:) double  {ismatrix, isnumeric}   % Actual size: (3, 3, ui32NumOfPoses)
+    dPositionCam_NavFrame       (3,:)   double  {ismatrix, isnumeric}   % Actual size: (3, ui32NumOfPoses)
+    ui32EstimationTimeID        (1,1)   double  {isscalar, isnumeric}
+    ui32NumOfPoses              (1,:)   uint32  {isscalar, isnumeric}
+    ui32MaxNumOfPoses           (1,1)   uint32  {isscalar, isnumeric} = ui32NumOfPoses;
+end
+%% PROTOTYPE
+%
+% -------------------------------------------------------------------------------------------------------------
+%% DESCRIPTION
+% What the function does
+% REFERENCE:
+% 1) High-precision, consistent EKF-based visual-inertial odometry, Li, Mourikis, 2023
+% 2) Vision-Aided Inertial Navigation for Spacecraft Entry, Descent, and Landing, Mourikis, 2009
+% 3) Statistical Orbit determination, Chapter 4, Tapley 2004
+% 4) Optimal State estimation: Kalman, H Infinity, and Nonlinear Approaches, Dan Simon, 2006
+% -------------------------------------------------------------------------------------------------------------
+%% INPUT
+% dDCM_NavFrameFromC          (3,3,:) double  {ismatrix, isnumeric}   % Actual size: (3, 3, ui32NumOfPoses)
+% dPositionCam_NavFrame       (3,:)   double  {ismatrix, isnumeric}   % Actual size: (3, ui32NumOfPoses)
+% ui32EstimationTimeID        (1,1)   double  {isscalar, isnumeric}
+% ui32NumOfPoses              (1,:)   uint32  {isscalar, isnumeric}
+% ui32MaxNumOfPoses           (1,1)   uint32  {isscalar, isnumeric} = ui32NumOfPoses;
+% -------------------------------------------------------------------------------------------------------------
+%% OUTPUT
+% dRelPos_CkFromCi_Ci
+% dDCM_CiFromCk
+% dPositionCk_NavFrame
+% dDCM_NavFrameFromCk
+% -------------------------------------------------------------------------------------------------------------
+%% CHANGELOG
+% 04-02-2025    Pietro Califano     Implement moving code from TriangulateFeaturesFromMotion.m
+% -------------------------------------------------------------------------------------------------------------
+%% DEPENDENCIES
+% [-]
+% -------------------------------------------------------------------------------------------------------------
+%% Future upgrades
+% 1) Replace WLS with Givens Rotations for improved efficiency TBC
+% 2) Modify code for static memory allocation
+% 3) Evaluate whether the function may be modified to enable only a subset (not continuous) set of poses to
+% be evaluated. I was thinking something like, boolean mask of size ui32MaxNumOfPoses, storing those into
+% the first entries of the buffer. The caller must known how to use those (e.g. allocating where needed).
+% -------------------------------------------------------------------------------------------------------------
+
+% Input checks
+assert( ui32NumOfPoses <= ui32MaxNumOfPoses);
+assert( ui32EstimationTimeID <= ui32NumOfPoses);
+
+% Instantiate temporary variables
+% dPixMeasResVec      = coder.nullcopy( zeros( size(dyMeasVec,1) * size(dyMeasVec,2), 1 ) );
+dRelPos_CkFromCi_Ci =  zeros(3, ui32MaxNumOfPoses) ;
+dDCM_CiFromCk       =  zeros(3, 3, ui32MaxNumOfPoses) ;
+
+%% Computation of relative camera pose Ci wrt Ck anchor
+
+% Get anchor pose
+dPositionCk_NavFrame = dPositionCam_NavFrame(1:3, ui32EstimationTimeID);
+dDCM_NavFrameFromCk  = dDCM_NavFrameFromC(:, :, ui32EstimationTimeID);
+
+% Compute poses relative to anchor pose
+for ui32IdPose = 1:ui32NumOfPoses
+
+    % Compute position of Ck wrt Ci in Ci camera frame
+    dDCM_CiFromNavFrame = transpose( dDCM_NavFrameFromC(:,:, ui32IdPose) );
+    dRelPos_CkFromCi_Ci(:, ui32IdPose) = dDCM_CiFromNavFrame * (dPositionCk_NavFrame - dPositionCam_NavFrame(1:3, ui32IdPose) );
+
+    % Compute attitudes of Ck wrt Ci camera frames (DCM from Ck to Ci)
+    dDCM_CiFromCk(:,:,ui32IdPose) = dDCM_CiFromNavFrame * dDCM_NavFrameFromCk;
+
+end
+
+
+end
