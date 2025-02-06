5a8ffef86fd118207456db11a87b7d0bbd418a49
PeterC <petercalifano.gs@gmail.com>
2025-02-06 19:09:42 +0100

    MInor changes (comments, TODOs)

--- matlab/sharedFiltersModules/dynamicsModels/filterDynOrbit_FixedEph.m
+++ matlab/sharedFiltersModules/dynamicsModels/filterDynOrbit_FixedEph.m
@@ -1,4 +1,4 @@
-function dxdt = filterDynOrbit(dStateTimetag, ...
+function dDxDt = filterDynOrbit_FixedEph(dStateTimetag, ...
                                 dxState, ...
                                 strDynParams, ...
                                 strStatesIdx) %#codegen
@@ -7,6 +7,7 @@ arguments
     dxState       (:, 1) double
     strDynParams  {isstruct}
     strStatesIdx  {isstruct}
+    % strFilterConstConfig
 end
 %% PROTOTYPE
 % dxdt = computeDynFcn(dStateTimetag,...
@@ -41,29 +42,22 @@ end
 % -------------------------------------------------------------------------------------------------------------
 %% Function code
 
+% Get configuration flags to configure dynamics 
+% TODO
+
+
 % TODO modify how this variable is used
-ui8NumOf3rdBodies = strDynParams.ui8NumOf3rdBodies;
+ui8NumOf3rdBodies = strFilterConstConfig.ui8NumOf3rdBodies; 
+% MUST come from filter const configuration. OTHERWISE: size of dBodyEphemerides, which will be FIXED.
 
 % Variables definition
-dxdt = zeros(size(dxState, 1), 1);
+dDxDt = zeros(size(dxState, 1), 1);
 
-% Check for 3rd bodies
+% Check for 3rd bodies % TODO: rework
 if not(isfield(strDynParams, 'strBody3rdData'))
     ui8NumOf3rdBodies = uint8(0);
 else
     ui8NumOf3rdBodies = uint8(length(strDynParams.strBody3rdData)); % TODO --> remove for static sizing
-end
-
-% Check validity of timetags
-
-if dStateTimetag <= strDynParams.strMainData.strAttData.dTimeLowBound
-    evalPoint = strDynParams.strMainData.strAttData.dTimeLowBound;
-
-elseif dStateTimetag >= strDynParams.strMainData.strAttData.dTimeUpBound
-    evalPoint = strDynParams.strMainData.strAttData.dTimeUpBound;
-
-else
-    evalPoint = dStateTimetag;
 end
 
 % Input checks and variables allocation
@@ -77,44 +71,25 @@ end
 % TODO: rework usage of ui16StatesIdx, which effectively is statically determined!
 ui16StatesIdx = [strStatesIdx.ui8posVelIdx(1), strStatesIdx.ui8posVelIdx(end)];
 
-% Compute attitude of Main at current time instant (NOT NEEDED IN FILTER)
-dDCMmainAtt_INfromTF  = coder.nullcopy(zeros(3, 3));
 
-dTmpQuat = evalAttQuatChbvPolyWithCoeffs(strDynParams.strMainData.strAttData.ui32PolyDeg, 4, evalPoint,...
-                                        strDynParams.strMainData.strAttData.dChbvPolycoeffs, ...
-                                        strDynParams.strMainData.strAttData.dsignSwitchIntervals, ...
-                                        strDynParams.strMainData.strAttData.dTimeLowBound, ...
-                                        strDynParams.strMainData.strAttData.dTimeUpBound);
+% dDCMmainAtt_INfromTF(1:3, 1:3) = Quat2DCM(dTmpQuat, true);
+dDCMmainAtt_INfromTF = zeros(3,3);
 
-dDCMmainAtt_INfromTF(1:3, 1:3) = Quat2DCM(dTmpQuat, true);
+% Compute SRP coefficient (TODO: rework to allow bias estimation)
+dBiasCoeffSRP = 0.0;
+% If bEstimatedCoeffSRP
+% dBiasCoeffSRP = dxState( );
+% end
 
-
-% Evaluate position Ephemerides of 3rd bodies
-dBodyEphemerides = coder.nullcopy(zeros(3*ui8NumOf3rdBodies, 1));
-d3rdBodiesGM = coder.nullcopy(zeros(ui8NumOf3rdBodies, 1));
-
-ptrAlloc = 1;
-
-for idB = 1:ui8NumOf3rdBodies
-
-    dBodyEphemerides(ptrAlloc:ptrAlloc+2) = evalChbvPolyWithCoeffs(strDynParams.strBody3rdData(idB).strOrbitData.ui32PolyDeg, ...
-                                                                 3, evalPoint,...
-                                                                 strDynParams.strBody3rdData(idB).strOrbitData.dChbvPolycoeffs, ...
-                                                                 strDynParams.strBody3rdData(idB).strOrbitData.dTimeLowBound, ...
-                                                                 strDynParams.strBody3rdData(idB).strOrbitData.dTimeUpBound);
-    
-    d3rdBodiesGM(idB) = strDynParams.strBody3rdData(idB).dGM;
-
-    ptrAlloc = ptrAlloc + 3;
-end
-
-% Compute SRP coefficient
 dCoeffSRP = (strDynParams.strSRPdata.dP_SRP * strDynParams.strSCdata.dReflCoeff * ...
              strDynParams.strSCdata.dA_SRP)/strDynParams.strSCdata.dSCmass; % Move to compute outside, since this
 
+dCoeffSRP = dCoeffSRP + dBiasCoeffSRP;
+
 %% Evaluate RHS
 % ACHTUNG: Sun must be first in ephemerides and GM data
-dxdt(strStatesIdx.ui8posVelIdx) = evalRHS_DynOrbit(dxState, ...
+% TODO make an "embeddable" version of evalRHS_DynOrbit --> no isempty checks
+dDxDt(strStatesIdx.ui8posVelIdx) = evalRHS_DynOrbit(dxState, ...
                                                        dDCMmainAtt_INfromTF, ...
                                                        strDynParams.strMainData.dGM, ...
                                                        strDynParams.strMainData.dRefRadius, ...
@@ -123,6 +98,8 @@ dxdt(strStatesIdx.ui8posVelIdx) = evalRHS_DynOrbit(dxState, ...
                                                        dBodyEphemerides, ...
                                                        strDynParams.strMainData.dSHcoeff, ...
                                                        ui16StatesIdx);
+
+% TODO: add dynamics 
 
 % dxdt(ui16StatesIdx(2,:)) = evalRHS_DynFOGM(dxState, ...
 %     dTimeConst, ...
