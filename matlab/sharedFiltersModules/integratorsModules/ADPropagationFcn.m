a5d09dc1d20ea542dae20c00ac2202bff3e55e2e
PeterC <petercalifano.gs@gmail.com>
2025-04-04 11:14:37 +0200

    Start work on AD integrator prototype

---
+++ matlab/sharedFiltersModules/integratorsModules/ADPropagationFcn.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = ADPropagator(inputArg1,inputArg2)
+%ADPROPAGATOR Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
