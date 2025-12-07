d48d170e4636163c692beddab4cb2e2f751dd5a7
PeterC <petercalifano.gs@gmail.com>
2025-12-07 16:01:27 +0100

    [MAJOR] Fix bug in computation of SRP jacobian
    - Move code to compute SRP pressure inside SRP jacobian function to force consistent jacobian
    
    - Fix incorrect jacobian (was accounting for 1/r^2 term twice)
    
    - Implement complete jacobian considering both magnitude and direction dependencies from estimated position (WIP)

--- matlab/sharedFiltersModules/dynamicsModels/JacobianModels/evalJAC_SRPwithBias.m
+++ matlab/sharedFiltersModules/dynamicsModels/JacobianModels/evalJAC_SRPwithBias.m
@@ -3,10 +3,10 @@ function [drvSRPwithBiasJac] = evalJAC_SRPwithBias(dxState, ...
                                                    strFilterMutabConfig, ...
                                                    strFilterConstConfig) %#codegen
 arguments
-    dxState
-    strDynParams
-    strFilterMutabConfig
-    strFilterConstConfig
+    dxState                 (:,1) double 
+    strDynParams            (1,1) struct
+    strFilterMutabConfig    (1,1) struct
+    strFilterConstConfig    (1,1) struct {coder.mustBeConst}
 end
 %% PROTOTYPE
 % [drvSRPwithBiasJac] = evalJAC_SRPwithBias(dxState, ...
@@ -19,9 +19,10 @@ end
 % ACHTUNG: this function currently assumes all inputs are in meters for computation of SRP coefficient.
 % -------------------------------------------------------------------------------------------------------------
 %% INPUT
-% dxState
-% strDynParams
-% strFilterConstConfig
+% dxState                 (:,1) double
+% strDynParams            (1,1) struct
+% strFilterMutabConfig    (1,1) struct
+% strFilterConstConfig    (1,1) struct {coder.mustBeConst}
 % -------------------------------------------------------------------------------------------------------------
 %% OUTPUT
 % drvSRPwithBiasJac
@@ -29,13 +30,13 @@ end
 %% CHANGELOG
 % 24-02-2025    Pietro Califano     First version implemented from evalJAC_DynLEO
 % 07-05-2025    Pietro Califano     Modify jacobian to include dependence of P_SRP from position
+% 07-12-2025    Pietro Califano     [MAJOR] Change interface and debug implementation of jacobian (was
+%                                           incorrectly including P_SRP dependence)
 % -------------------------------------------------------------------------------------------------------------
 %% DEPENDENCIES
 % [-]
 % -------------------------------------------------------------------------------------------------------------
-%% Future upgrades
-% [-]
-% -------------------------------------------------------------------------------------------------------------
+
 %% Function code
 
 % Get indices for allocation
@@ -61,33 +62,56 @@ else
     drvSRPwithBiasJac = zeros(6, 3);
 end
 
+%% Compute distance from the Sun and P_SRP
+dSunPositionFromSC_IN       = strDynParams.dBodyEphemerides(1:3) - dxState(ui8PosVelIdx(1:3));
+dNormSunPositionFromSC_IN   = norm( dSunPositionFromSC_IN );
+dInvNormSunPositionFromSC   = 1/dNormSunPositionFromSC_IN;
+
+if dNormSunPositionFromSC_IN <= 1e10
+    % Assumes km scale
+    dAU = coder.const(1.495978707E8);
+    strDynParams.strSRPdata.dP_SRP0 = coder.const(1E3 * 1367 / 299792458);
+else
+    % Assumes m scale
+    dAU = coder.const(1.495978707E11);
+    
+    strDynParams.strSRPdata.dP_SRP0 = coder.const(1367 / 299792458); % Approx. 4.54e-6 N/m^2
+end
+
+% Compute AU^2 as coder constant
+dAU2 = coder.const(dAU * dAU);
+
+% Compute SRP value from SRP0 at 1AU
+strDynParams.strSRPdata.dP_SRP = strDynParams.strSRPdata.dP_SRP0 * (dAU2 * (dInvNormSunPositionFromSC^2)); % [N/m^2] or [N/km^2]
+
 %% Compute jacobian wrt SRP acceleration
-dSunPositionFromSC_IN = strDynParams.dBodyEphemerides(1:3) - dxState(ui8PosVelIdx(1:3));
-dNormSunPositionFromSC_IN = norm( dSunPositionFromSC_IN );
-
 % DEVNOTE this coefficient is recomputed here, instead of re-using calculation from propagateDyn
-% TODO modify to recompute P_SRP depending on the spacecraft position
-
 dCoeffSRP = (strDynParams.strSRPdata.dP_SRP * strDynParams.strSCdata.dReflCoeff * ...
              strDynParams.strSCdata.dA_SRP)/strDynParams.strSCdata.dSCmass; % Move to compute outside, since this
 
-% Compute Jacobian of position and velocity
+%%% Compute Jacobian of position and velocity
+% Jacobian neglecting dependence of P_SRP due to position
 % drvSRPwithBiasJac(ui8PosVelIdx(4:6), 1:3) = - ( dCoeffSRP + dBiasCoeff ) * ( (1 / dNormSunPositionFromSC_IN) * eye(3)  ...
 %                                                                              - (1 /( dNormSunPositionFromSC_IN^3 )) ...
 %                                                                                  * (dSunPositionFromSC_IN * dSunPositionFromSC_IN') ) ; % [6x3]
 
-dInvNormSunPositionFromSC = 1/dNormSunPositionFromSC_IN;
+% Compute auxiliary coefficient
 dInvNormSunPositionFromSC3 = dInvNormSunPositionFromSC^3;
-dInvNormSunPositionFromSC5 = dInvNormSunPositionFromSC^5;
 
-drvSRPwithBiasJac(ui8PosVelIdx(4:6), 1:3) = - ( dCoeffSRP + dBiasCoeff ) * ( dInvNormSunPositionFromSC3 * eye(3)  ...
-                                                  - 3*dInvNormSunPositionFromSC5 * (dSunPositionFromSC_IN * transpose(dSunPositionFromSC_IN)) ) ; % [3x3]
+% Complete jacobian including dependence of P_SRP from spacecraft position
+% Bias is assumed independent of position
+drvSRPwithBiasJac(ui8PosVelIdx(4:6), 1:3) = - ( ( (dCoeffSRP + dBiasCoeff)*dInvNormSunPositionFromSC * eye(3)  ...
+                                                      - (3*dCoeffSRP + dBiasCoeff)*( dInvNormSunPositionFromSC3 * ...
+                                                                (dSunPositionFromSC_IN * transpose(dSunPositionFromSC_IN)) ) ) ); % [3x3]
+
+
+
 
 %% Compute jacobian wrt SRP bias coefficient
 if strFilterMutabConfig.bEnableBiasSRP && coder.const(ui8CoeffSRPidx > 0)
     % DEVNOTE not sure if need to be disabled because in principle the stochastic process affecting the C_SRP
     % coefficient does not enter the deterministic part of the dynamics (hence in A).
-    dJacCoeffSRP = - (dCoeffSRP + 1.0) * dInvNormSunPositionFromSC * dSunPositionFromSC_IN;
+    dJacCoeffSRP = - (dCoeffSRP + 1.0) * (dInvNormSunPositionFromSC * dSunPositionFromSC_IN);
     drvSRPwithBiasJac(4:6, 4) = dJacCoeffSRP; % [6x1]
 end
 
