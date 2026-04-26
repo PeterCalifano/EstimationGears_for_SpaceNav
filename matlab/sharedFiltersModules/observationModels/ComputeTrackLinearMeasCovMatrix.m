9ff5f7253fc61a9474c72be3b56e675b825f373e
PeterC <petercalifano.gs@gmail.com>
2026-04-26 19:55:32 +0200

    Move functions from ekf_modules to generic folders

---
+++ matlab/sharedFiltersModules/observationModels/ComputeTrackLinearMeasCovMatrix.m
@@ -0,0 +1,77 @@
+function [dRmeasCovMatrix, dMaxKeypointSigma] = ComputeTrackLinearMeasCovMatrix(dModelTrackLength, ...
+                                                                                dMinMaxKeypointSigma, ...
+                                                                                dTrackLength, ...
+                                                                                ui32MaxTrackSize, ...
+                                                                                dKcam, ...
+                                                                                bNormCoordsCov)%#codegen
+arguments
+    dModelTrackLength     (1,1) double {isscalar} = 20;
+    dMinMaxKeypointSigma  (2,1) double {isvector} = [0.25; 2];
+    dTrackLength          (1,1) double {isscalar} = 1;
+    ui32MaxTrackSize      (1,1) uint32 {isscalar} = dTrackLength;
+    dKcam                 (3,3) double            = zeros(3,3);
+    bNormCoordsCov        (1,1) logical {isscalar, islogical} = false;
+end
+%% SIGNATURE
+% [dRmeasCovMatrix, dMaxKeypointSigma] = ComputeTrackLinearMeasCovMatrix(dModelTrackLength, ...
+%                                                                        dMinMaxKeypointSigma, ...
+%                                                                        dTrackLength, ...
+%                                                                        ui32MaxTrackSize)%#codegen
+% -------------------------------------------------------------------------------------------------------------
+%% DESCRIPTION
+% Function computating the (diagonal) measurement noise covariance matrix of keypoints measurements assuming
+% a linear model with feature track length (tailored for feature tracking algorithms, to counteract drift).
+% -------------------------------------------------------------------------------------------------------------
+%% INPUT
+% dModelTrackLength     (1,1) double {isscalar} = 20;
+% dMinMaxKeypointSigma  (2,1) double {isvector} = [0.25; 2];
+% dTrackLength          (1,1) double {isscalar} = 1;
+% ui32MaxTrackSize      (1,1) uint32 {isscalar} = dTrackLength;
+% -------------------------------------------------------------------------------------------------------------
+%% OUTPUT
+% dRmeasCovMatrix
+% dMaxKeypointSigma
+% -------------------------------------------------------------------------------------------------------------
+%% CHANGELOG
+% 03-02-2025        Pietro Califano         Implemented from legacy code for GTSAM projection factor.
+% -------------------------------------------------------------------------------------------------------------
+%% DEPENDENCIES
+% [-]
+% -------------------------------------------------------------------------------------------------------------
+%% Function code
+
+% Define output matrix
+dRmeasCovMatrix = zeros(2*ui32MaxTrackSize);
+
+% Define linear model for the feature track keypoints variance
+ui32AllocIndex = 1;
+
+if bNormCoordsCov
+    dVarScaleX = 1./(dKcam(1,1)^2);
+    dVarScaleY = 1./(dKcam(2,2)^2);
+end
+
+for idL = 1:dTrackLength
+
+    % Compute sigma based on track length
+    dVarPix = (dMinMaxKeypointSigma(1) + (dMinMaxKeypointSigma(2) - dMinMaxKeypointSigma(1)) * ...
+                        (idL - 1) / dModelTrackLength)^2;
+    
+    % Allocate diagonal entries
+    if bNormCoordsCov
+        dRmeasCovMatrix(ui32AllocIndex, ui32AllocIndex)     = dVarScaleX * dVarPix;
+        dRmeasCovMatrix(ui32AllocIndex+1, ui32AllocIndex+1) = dVarScaleY * dVarPix;
+    else
+        dRmeasCovMatrix(ui32AllocIndex, ui32AllocIndex)     = dVarPix;
+        dRmeasCovMatrix(ui32AllocIndex+1, ui32AllocIndex+1) = dVarPix;
+    end
+
+    ui32AllocIndex = ui32AllocIndex + 2;
+end
+
+if nargout > 1
+    dMaxKeypointSigma = max(dRmeasCovMatrix, 'all');
+end
+
+
+end
---
+++ matlab/sharedFiltersModules/slidingWindow/ComputeWindowPoseJacobian.m
@@ -0,0 +1,70 @@
+function [dJacPoseCovFromState] = ComputeWindowPoseJacobian(dxState, ...
+                                                            dQuat_TBfromIN, ...
+                                                            dQuat_TBfromCam, ...
+                                                            strFilterMutabConfig, ...
+                                                            strFilterConstConfig) %#codegen
+arguments
+    dxState         % TBC if not needed
+    dQuat_TBfromIN          (1,4)
+    dQuat_TBfromCam         (1,4)
+    strFilterMutabConfig    (1,1) {isstruct}
+    strFilterConstConfig    (1,1) {isstruct}
+end
+%% SIGNATURE
+% [dCamPosition_TB, dQuat_TBfromCam] = ComputeWindowPose(dCamPosition_IN, ...
+%                                                        dxAttitudeBiasStates, ...
+%                                                        dQuat_INfromSC, ...
+%                                                        dQuat_TBfromIN, ...
+%                                                        dQuat_SCfromCam) %#codegen
+% -------------------------------------------------------------------------------------------------------------
+%% DESCRIPTION
+% Function computing the jacobian of the transformation from current position, attitude bias and attitude 
+% quaternion to window camera pose, assuming the camera origin coincident with spacecraft origin.
+% -------------------------------------------------------------------------------------------------------------
+%% INPUT
+% in1 [dim] description TODO
+% -------------------------------------------------------------------------------------------------------------
+%% OUTPUT
+% out1 [dim] description
+% Name1                     []
+% Name2                     []
+% Name3                     []
+% -------------------------------------------------------------------------------------------------------------
+%% CHANGELOG
+% 05-02-2025    Pietro Califano     First prototype implementation for MSCKF.
+% -------------------------------------------------------------------------------------------------------------
+%% DEPENDENCIES
+% [-]
+% -------------------------------------------------------------------------------------------------------------
+%% Function code
+dJacPoseCovFromState = zeros( strFilterConstConfig.ui16WindowPoseSize-1, strFilterConstConfig.ui16StateSize);
+
+if coder.target('MATLAB') || coder.target('MEX')
+    assert(strFilterMutabConfig.i8FeatTrackingMode == 0 || strFilterMutabConfig.i8FeatTrackingMode == 1 || strFilterMutabConfig.bContinuousSlideMode)
+    mustBeMember(strFilterMutabConfig.charWindowRefFrame, ["TB", "IN"]);
+end
+
+if strcmpi(strFilterMutabConfig.charWindowRefFrame, 'TF')
+    % Mode 0: Tightly coupled feature tracking mode (MSCKF) --> Window poses in target fixed frame.
+
+    % Compute Jacobian of window camera position wrt state
+    % NOTE: jacobian of target fixed position wrt inertial state
+    dJacPoseCovFromState(1:3, strFilterConstConfig.strStatesIdx.ui8posVelIdx(1:3)) = Quat2DCM(dQuat_TBfromIN, false); 
+    
+    % Compute Jacobian of window camera attitude (from target fixed) wrt state 
+    % NOTE: Jacobian of attitude quaternion Cam from Fixed wrt attitude bias (error true TB from est TB)
+    dJacPoseCovFromState(4:6, strFilterConstConfig.strStatesIdx.ui8attBiasDeltaIdx) = Quat2DCM(dQuat_TBfromIN, false); % TODO review
+
+elseif strcmpi(strFilterMutabConfig.charWindowRefFrame, 'IN')
+    % Mode 1: Loosely coupled feature tracking mode (Direction of motion) --> Position in Inertial frame, attitude wrt target fixed frame
+    dJacPoseCovFromState(1:3, strFilterConstConfig.strStatesIdx.ui8posVelIdx(1:3)) = eye(3);
+
+    % Compute Jacobian of window camera attitude (from target fixed) wrt state
+    dJacPoseCovFromState(4:6, strFilterConstConfig.strStatesIdx.ui8attBiasDeltaIdx) = Quat2DCM(dQuat_TBfromIN, false); % TODO
+else
+    if coder.target('MATLAB') || coder.target('MEX')
+        assert(0, 'Invalid pose augmentation mode.')
+    end
+end
+
+end
