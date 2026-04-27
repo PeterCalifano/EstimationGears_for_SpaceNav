0ce267cd6edc9d529aaee3e5ed41f3fe0c66ed67
PeterC <petercalifano.gs@gmail.com>
2026-04-27 12:30:09 +0200

    Add enum for estimator backend

--- matlab/+filter_tailoring/BuildArchitectureTemplate.m
+++ matlab/+filter_tailoring/BuildArchitectureTemplate.m
@@ -4,14 +4,19 @@ arguments
     kwargs.charDefsOutputPath   {mustBeA(kwargs.charDefsOutputPath, ["string", "char"])} = "./bus_defs_EKF";
     kwargs.ui16StateSize        (1,1) uint16 = 17
     kwargs.bAddExponentialAtmosphData (1,1) logical = false
+    kwargs.enumFilterBackend    (1,1) EnumFilterBackend = EnumFilterBackend.EKF_FULLCOV
+    kwargs.enumSmoothingBackend (1,1) EnumSmoothingBackend = EnumSmoothingBackend.NONE
 end
 %% CHANGELOG
 % 22-04-2026    Pietro Califano     Add reusable default architecture builder for standard filter structs.
 % 24-04-2026    Pietro Califano     Align sigma-point architecture metadata with the EKF-style template.
 % 24-04-2026    Pietro Califano     Align sigma-point runtime fields with the EKF-style template.
 % 26-04-2026    Pietro Califano     Remove template-mode selector; keep only explicit optional data flags.
+% 27-04-2026    Pietro Califano     Add const-config backend and smoothing selectors.
 
 strFilterConstConfig.ui16StateSize = uint16(kwargs.ui16StateSize);
+strFilterConstConfig.enumFilterBackend = kwargs.enumFilterBackend;
+strFilterConstConfig.enumSmoothingBackend = kwargs.enumSmoothingBackend;
 strFilterConstConfig.ui32UnscentedNumSigmaPoints = uint32(2 * double(strFilterConstConfig.ui16StateSize) + 1);
 strFilterConstConfig.dUnscentedAlpha            = 1.0e-3;
 strFilterConstConfig.dUnscentedBeta             = 2.0;
