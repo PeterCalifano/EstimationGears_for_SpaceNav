e498efca3825fa61b242e0d0d75af7a56084201e
PeterC <petercalifano.gs@gmail.com>
2026-04-27 11:43:52 +0200

    Add new enum classes

---
+++ matlab/datastructs/EnumFilterBackend.m
@@ -0,0 +1,6 @@
+classdef EnumFilterBackend < uint32
+  enumeration
+      EKF_FULLCOV (0)
+      SRIF (1)
+  end
+end
---
+++ matlab/datastructs/EnumSmoothingBackend.m
@@ -0,0 +1,7 @@
+classdef EnumSmoothingBackend < uint32
+  enumeration
+      NONE (0)
+      SRIS_SLIDEWINDOW (1)
+      SRIS_FULLHISTORY (2)
+  end
+end
